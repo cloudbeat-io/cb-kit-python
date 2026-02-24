872e7d2b3d28b01c1507b210f7c5454a4aec3661
romovs <romovs@gmail.com>
2026-02-24 12:42:03 +0200

    Code cleanup

--- cb-kit-common/cloudbeat.py
+++ cb-kit-common/cloudbeat.py
@@ -2,18 +2,9 @@ from cloudbeat_common.models import TestStatus
 from cloudbeat_common.reporter import CbTestReporter
 from cloudbeat_common.client import (
     CbApiError,
-    RunOptions,
-    RunStatus,
-    RunInstanceStatus,
-    RunInstanceCaseStatus,
-    TestCaseTagDto,
-    ProjectSyncStatus,
     RunStatusInfo,
     CaseStatusUpdateReq,
     SuiteStatusUpdateReq,
-    RuntimeApiV1,
-    ResultApiV1,
-    ProjectApiV1,
     RuntimeApiV2,
 )
 
@@ -23,17 +14,8 @@ __all__ = [
     'CbTestReporter',
     # Client API
     'CbApiError',
-    'RunOptions',
-    'RunStatus',
-    'RunInstanceStatus',
-    'RunInstanceCaseStatus',
-    'TestCaseTagDto',
-    'ProjectSyncStatus',
     'RunStatusInfo',
     'CaseStatusUpdateReq',
     'SuiteStatusUpdateReq',
-    'RuntimeApiV1',
-    'ResultApiV1',
-    'ProjectApiV1',
     'RuntimeApiV2',
 ]
